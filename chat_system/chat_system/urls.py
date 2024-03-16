94b0eb80c5211c181cef8482b4b5cd48700d7939
Nikx908 <nikhilparbat908@gmail.com>
2024-03-17 01:21:41 +0530

    edits

--- chat_system/chat_system/urls.py
+++ chat_system/chat_system/urls.py
@@ -4,6 +4,7 @@ URL configuration for chat_system project.
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
 Examples:
+
 Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
@@ -16,6 +17,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
