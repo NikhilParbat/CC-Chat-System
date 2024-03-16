81e27aa845fe00f57c638b0a78ca78b95de9ac3b
Nikx908 <nikhilparbat908@gmail.com>
2024-03-17 01:08:59 +0530

    Migrations and Database

--- chat_system/chat_system/settings.py
+++ chat_system/chat_system/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'chat_system.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql_psycopg2',
+       'NAME': 'pwgquhvs',
+       'USER': 'pwgquhvs',
+       'PASSWORD': '40190V0BXHsMxra3VuMMTM1C521dAfU5',
+       'HOST': 'floppy.db.elephantsql.com',
+       'PORT': '5432',
+   }
 }
+
 
 
 # Password validation
